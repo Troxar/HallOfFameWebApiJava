468480861034c25e45367613a2580f4a29843678
Troxar <afargoth@gmail.com>
2024-08-27 22:25:23 +0700

    Add project

---
+++ src/main/java/HallOfFame/WebApi/HallOfFameWebApiApplication.java
@@ -0,0 +1,13 @@
+package HallOfFame.WebApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallOfFameWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallOfFameWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HallOfFame/WebApi/HallOfFameWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package HallOfFame.WebApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallOfFameWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
